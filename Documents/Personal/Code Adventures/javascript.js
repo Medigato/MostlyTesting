33d745b71429a88a95e6a18a53710ae2b0d579a9
Madelaine <madelainemartinis@gmail.com>
2021-10-26 13:26:30 -0500

    First Version HTML Files

--- Documents/Personal/Code Adventures/javascript.js
+++ Documents/Personal/Code Adventures/javascript.js
@@ -8,6 +8,17 @@
            var smile = 0;
            var totalSmileOutput = 0;
 
+
+           //story stuff
+
+           var mainStoryText;
+           var mainStoryArray = ["Your world is dizziness and confusion.","The warmth, welcoming darkness you had been used to appeared to be over, you breathe your first breath.","Then immediately start bawling your heart out. ","The first months are chaos, but after many moons pass, you recognize something in the shadows. ",
+           "You extend your hand and grab your first fiber of understanding. The face amongst the blurriness is looking directly at you. ",
+          "And they're smiling.",
+         "Suddenly, you feel an irrestible urge."];
+           var mainStoryNumber = 0;
+       
+
         totalSmileoutputf();
             // window.setInterval(function() { }, every how many ms. 
             //refresh score every 200 ms
@@ -22,7 +33,22 @@
 
             //refresh ffps
             document.getElementById("ffpsCount").innerHTML = "Currently generating " + smile + " fireflies per second.";
-    
+
+            if (ff > 15) {
+                
+                document.getElementById("childhoodActivities").style.display = "inline";
+                
+
+            }
+
+            if (ff > 10) {
+                document.getElementById("continueStory").style.display = "inline";
+                mainStoryArray = ["It is the face of your mother, so unearthly among the shadows. ","Her lips are rose pink and her eyes are piercing hazel.", "Consciousness forms fast like a thunder storm.", "What would you do with your time?"];
+                // Add a Table with 4 buttons childhood activities. 
+                        
+                
+
+            }
 
 
 
@@ -54,11 +80,55 @@
         
             }
 
-        function clear1ps() {
-
-            
+        function clear1ps() {           
 
             smile -= 1;
 
 
         }
+
+
+        function continueStory(){
+              
+                        
+            document.getElementById("mainStoryText").innerHTML = mainStoryArray[mainStoryNumber];
+            mainStoryNumber += 1;
+            
+            if (mainStoryNumber === mainStoryArray.length){
+            document.getElementById("continueStory").style.display = "none";
+            enableSmile();
+            mainStoryNumber = 0;
+            document.getElementById("goals").innerHTML = "Goal: Reach 10 Fireflies." 
+            
+            }
+
+
+
+        }
+
+        function enableSmile() {
+
+            document.getElementById("add1ps").style.display="block";
+
+
+        }
+        function once(fn, context) { 
+            var result;
+            return function() { 
+                if (fn) {
+                    result = fn.apply(context || this, arguments);
+                    fn = null;
+                }
+                return result;
+            };
+        }
+
+ 
+        if (ff > 10) {
+            document.getElementById("continueStory").style.display = "block";
+            mainStoryArray = ["It is the face of your mother, so unearthly among the shadows. ","Her lips are rose pink and her eyes are piercing hazel.", "Consciousness forms fast like a thunder storm.", "What would you do with your time?"];
+            // Add a Table with 4 buttons childhood activities. 
+            
+
+                 }
+      

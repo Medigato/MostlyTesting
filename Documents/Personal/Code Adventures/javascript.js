3c246876360f3f6794d72cc4df23d8562657af78
Madelaine <madelainemartinis@gmail.com>
2021-11-03 11:12:32 -0600

    Layout Updated

--- Documents/Personal/Code Adventures/javascript.js
+++ Documents/Personal/Code Adventures/javascript.js
@@ -1,12 +1,12 @@
     //variables to be used in script 
 
             //currencies
-            var ff = 0;
+                 var ff = 0;
 
            //factories 
 
-           var smile = 0;
-           var totalSmileOutput = 0;
+                var smile = 0;
+                var totalSmileOutput = 0;
 
 
            //story stuff
@@ -14,26 +14,26 @@
            var mainStoryText;
            var mainStoryArray = 
            [
-               
-            ["Your world is dizziness and confusion.","The warmth, welcoming darkness you had been used to appeared to be over, you breathe your first breath.","Then immediately start bawling your heart out. ","The first months are chaos, but after many moons pass, you recognize something in the shadows. ",
+                  
+                 ["Your world is dizziness and confusion.","The warmth, welcoming darkness you had been used to appearo to be over, you breathe your first breath.","Then immediately start bawling your heart out. ","The first months are chaos, but after many moons pass, you recognize something in the shadows. ",
                         "You extend your hand and grab your first fiber of understanding. The face amongst the blurriness is looking directly at you. ",
                         "And they're smiling.",
                         "Suddenly, you feel an irrestible urge."],
 
-         ["It is the face of your mother, so unearthly among the shadows. ","Her lips are rose pink and her eyes are piercing hazel.", "Consciousness forms fast like a thunder storm.", "What would you do with your time?"]]
+                ["It is the face of your mother, so unearthly among the shadows. ","Her lips are rose pink and her eyes are piercing hazel.", "Consciousness forms fast like a thunder storm.", "What would you do with your time?"]]
 
              var currentStoryTextNumber = 0;    
              var mainStoryNumber = 0;
-           var currentStoryArray = mainStoryArray[currentStoryTextNumber];
+             var currentStoryArray = mainStoryArray[currentStoryTextNumber];
            
        
 
-        totalSmileoutputf();
-            // window.setInterval(function() { }, every how many ms. 
-            //refresh score every 200 ms
+              totalSmileoutputf();
+                
         window.setInterval(function () {
-
-            //refresh score
+                // window.setInterval(function() { }, every how many ms. 
+                //refresh score every 200 ms
+                //refresh score
             document.getElementById("ffCount").innerHTML = "You have " + ff + " fireflies!";
 
         }, 200);
@@ -48,8 +48,8 @@
             }
 
             if (ff > 10) {
-                document.getElementById("continueStory").style.display = "inline";
-               
+                document.getElementById("continueStory").style.display = "block";
+             
             
                         
                 
@@ -101,10 +101,10 @@
             mainStoryNumber += 1;
             
             if (mainStoryNumber === currentStoryArray.length){
-            document.getElementById("continueStory").style.display = "none";
-            currentStoryTextNumberfunction(currentStoryTextNumber);
-            mainStoryNumber = 0;
-            document.getElementById("goals").innerHTML = "Goal: Reach 10 Fireflies." 
+                document.getElementById("continueStory").style.display = "none";
+                currentStoryTextNumberfunction(currentStoryTextNumber);
+                mainStoryNumber = 0;
+                
             
             }
 
@@ -115,6 +115,7 @@
         function currentStoryTextNumberfunction(){
             if(currentStoryTextNumber===1){
                 enableChildHood();
+                 
         }
             if(currentStoryTextNumber===0){
                     enableSmile();
@@ -141,8 +142,8 @@
         function enableChildHood(){
 
 
-            document.getElementById("childhoodActivities").style.display = "inline";
-                
+            document.getElementById("activitiesContainer").style.display = "block";
+            document.getElementById("goals").innerHTML = "Goal: Reach 10 Fireflies."
 
 
 
